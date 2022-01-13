9d3b20b712424ad6e593d1e0ee5fe543f355bd94
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-01-13 21:20:20 +0300

    check max person count logic implemented

--- src/main/java/com/gudev/productercase/repository/PlayerRepository.java
+++ src/main/java/com/gudev/productercase/repository/PlayerRepository.java
@@ -2,8 +2,15 @@ package com.gudev.productercase.repository;
 
 import com.gudev.productercase.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+
+    @Query("select p.position from Player p")
+    List<String> getBusyPositions();
+
 }
