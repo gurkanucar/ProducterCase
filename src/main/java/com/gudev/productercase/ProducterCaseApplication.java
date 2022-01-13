c92cd180ead75408631d45034f2419abbbd73afd
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-01-13 19:18:29 +0300

    Initial commit

---
+++ src/main/java/com/gudev/productercase/ProducterCaseApplication.java
@@ -0,0 +1,13 @@
+package com.gudev.productercase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducterCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducterCaseApplication.class, args);
+    }
+
+}
