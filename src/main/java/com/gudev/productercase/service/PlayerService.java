ca79a87513644359d1bfcddd9184d5abea2eca2e
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-01-13 23:43:27 +0300

    small bug fixed

--- src/main/java/com/gudev/productercase/service/PlayerService.java
+++ src/main/java/com/gudev/productercase/service/PlayerService.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Service
 public class PlayerService {
 
-    @Value("maxPlayerCount")
+    @Value("${maxPlayerCount}")
     private int maxPlayerCount;
 
     private final PlayerRepository playerRepository;
